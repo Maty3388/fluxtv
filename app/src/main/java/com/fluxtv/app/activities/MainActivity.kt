de7aa9dccfc2022e892c84421bbf89fc8e2e192d
Maty3388 <Eithan100522@gmail.com>
2026-06-13 15:53:32 -0300

    fix: import RecyclerView en MainActivity

--- app/src/main/java/com/fluxtv/app/activities/MainActivity.kt
+++ app/src/main/java/com/fluxtv/app/activities/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
