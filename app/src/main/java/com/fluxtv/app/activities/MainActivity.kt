63c1cfd8006b3b47f61fed0da3c4603dffce5343
Maty3388 <Eithan100522@gmail.com>
2026-06-13 21:32:45 -0300

    fix: re-agregar import Toast

--- app/src/main/java/com/fluxtv/app/activities/MainActivity.kt
+++ app/src/main/java/com/fluxtv/app/activities/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
+import android.widget.Toast
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
